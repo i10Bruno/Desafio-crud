a26ea4b24f73d01878de1bdae689d97682efc5c8
Bruno alexandre <brunosantosalexandre@hotmail.com>
2025-07-21 11:03:40 -0300

    first commit

---
+++ src/main/java/com/desafiocrudjava/desafio/entities/Client.java
@@ -0,0 +1,15 @@
+package com.desafiocrudjava.desafio.entities;
+
+import java.time.LocalDate;
+
+public class Client {
+
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    private LocalDate birthDate;
+    private Integer children;
+
+}
---
+++ src/test/java/com/desafiocrudjava/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiocrudjava.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
